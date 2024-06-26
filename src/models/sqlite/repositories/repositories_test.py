2ad6713cd13a336f7c374f233646d9e1385c6020
Victor Raony <victor_raony12@hotmail.com>
2024-06-26 16:16:26 -0300

    feat: Implementing people_repository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,8 +1,9 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
-#db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="Intergração com banco de dados")
 def test_list_pets():
@@ -15,4 +16,13 @@ def test_delete_pet():
     name = "cobra"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="Intergração com banco de dados")
+def test_insert_person():
+    first_name = "test_name"
+    last_name = "test_last_name"
+    age = 77
+    pet_id = 2
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
     
